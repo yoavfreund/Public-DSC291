b3b2ac151c247f9f5b32ced28ded26fe7861327b
yoav freund <yfreund@ucsd.edu>
2020-04-21 16:06:33 -0700

    reverted to edX MM version of PCA notebook

--- notebooks/Section2-PCA/PCA/data_preparation/Partition stations into a Tree./utils/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## This file needs to be executed in the ec2 instance before launching
-## the notebook
-######### Settings for UCSD_BigData #######################
-## Vault directory for LanchNotebookServer.py
-export EC2_VAULT='/Users/yoavfreund/BigData/Vault'
-## Settings for emr uploads
-export BD_GitRoot='/Users/yoavfreund/BigData/UCSD_BigData'
-###########################################################
