3a4b928bcaa81dbda20e9cd0c5dbedb7ddb4c24e
yoav freund <yfreund@ucsd.edu>
2020-06-03 11:53:18 -0700

    copied Sections 4,5 from edx-micromasters

--- notebooks/Section4-Classification/XGBoost/lib/spark_PCA.py
+++ notebooks/Section4-Classification/XGBoost/lib/spark_PCA.py
@@ -29,7 +29,7 @@ def computeCov(RDDin):
     # print 'N=',N
     E=S[0,1:]
     NE=np.float64(N[0,1:])
-    print('shape of E=',E.shape,'shape of NE=',NE.shape)
+    # print('shape of E=',E.shape,'shape of NE=',NE.shape)
     Mean=E/NE
     O=S[1:,1:]
     NO=np.float64(N[1:,1:])
